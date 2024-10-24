2dd8fcc857cb3aa638d93048a08c3e99276bd495
snubsteel <141686887+snubsteel@users.noreply.github.com>
2024-10-24 01:05:44 -0500

    0.01

---
+++ main/Main.java
@@ -0,0 +1,23 @@
+package main;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+    public static void main(String[]args) {
+
+        JFrame window = new JFrame ("Java Chess");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        // Add gamePanel to the window
+        GamePanel gp = new GamePanel();
+        window.add(gp);
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gp.launchGame();
+    }
+}
